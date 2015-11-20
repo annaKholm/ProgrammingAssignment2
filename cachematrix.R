6e346c1b76e0b43d6032a37acbefc72056b83d76
annaKholm <anechkakh@gmail.com>
2015-11-20 14:21:03 +0200

    Fixed comments of the code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,12 @@
-#makeCacheMatrix takes a matrix and saves in the private variable x
+# Matrix inversion is a time-consuming task. Sometimes this task appears several times in the
+# given process. In order to avoid repeated computations the cache version of the original matrix as well as its inversion are stored 
+# in the cache. 
+
+# In this case each time when in the course of the process there a need to carry out matrix inversion 
+# the program checks and compares the matrix to be inversed with the one stored in the cache.
+# If the are identical, then instead of inversing the matrix once more it retrieves the inversed matrix from the cache.   
+
+# makeCacheMatrix takes a matrix and saves it in the private variable x
 # matr - a cached matrix
 #get - get function for the matrix
 #set - set function, reset matrix matr to NULL
@@ -18,14 +26,11 @@ makeCacheMatrix <- function(x = matrix()) {
          getsolve = getsolve)
    }
 
-
-
-# cachemean takes a caching matrix created with makeCacheMatrix
-# if we've already computed the inverse and stored it via makeCacheMatrix(),
+# cachemean takes a cached matrix created with makeCacheMatrix
+# if we've already computed the inversed matrix and stored it via makeCacheMatrix(),
 # and have not invalidated the cache by calling set(), return the cached
 # version of x  
-# else -  set the inverse in x so we cache it and dont need to needlessly
-# recompute it
+# else -  set the inverse in x so we cache it and would not need to recompute it in the future
 #  x$setsolve(matr) returns the caching matrix matr
 cacheSolve <- function(x, ...) {
     matr <- x$getsolve()
